369834b46f92b06ec84522ff430448d0b0750915
abdulwasay <abdulwasayhashmi983@gmail.com>
2024-08-01 05:40:34 +0500

    third commit

--- script.ts
+++ script.ts
@@ -1,4 +1,4 @@
- //union and intersection type
+ //union type
   let variable: string | null;
   variable = null;
   variable ="wasay"
@@ -13,3 +13,27 @@ function abcd(variable1 : number | string){
 variable1.toUpperCase()
     }
 }
+abcd(12)
+abcd("12")
+// so now both will work in that case
+// any other type will not work like this
+// abcd(true)
+
+// intersection type so we want both or trwice type like this
+type City = {
+    name:string,
+    population:number
+}
+type Planet = {
+    presnt:boolean
+}
+
+type CityInPlanet = City & Planet;
+
+let value: CityInPlanet = {
+    name:"wasay",
+    population:1224242,
+    presnt:true
+}
+
+// so that in this case we can provide all data of give type and we can intersection defferent types togather
