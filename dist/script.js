71f1d1f13a0125b37e0895b2682c5e1b6d0aba2a
abdulwasay <abdulwasayhashmi983@gmail.com>
2024-08-02 06:42:21 +0500

    access modifier commit

--- dist/script.js
+++ dist/script.js
@@ -1,17 +1,82 @@
 "use strict";
-// so both things are merge together in memory
-function abcd(human) {
-    human.name.concat();
-    human.age.toExponential(2);
+// class object oriented paradigm
+let button1 = document.getElementById("button1");
+let button2 = document.getElementById("button2");
+class Airpod {
+    constructor() {
+        this.color = "white";
+        this.picture = "image/airpod";
+    }
+    playMusic() {
+        console.log("Playing music");
+    }
 }
-function getMethaFood(mithai) {
-    mithai.name;
-    mithai.price;
-    mithai.verySweet;
+class airconditioner {
+    constructor() {
+        this.color = "white";
+        this.ton = .75;
+        this.temp = 22;
+    }
+    increaseTemp() {
+        this.temp++;
+        console.log(this.temp);
+    }
+    decreaseTemp() {
+        this.temp--;
+        console.log(this.temp);
+    }
+    turningOn(name) {
+        console.log(`turned on ${name} `);
+    }
 }
-function getKhattaFood(khattai) {
-    khattai.name;
-    khattai.price;
-    khattai.verySour;
+let ac1 = new airconditioner();
+let ac2 = new airconditioner();
+ac1.turningOn("ac1");
+if (button1) {
+    button1.addEventListener('click', () => ac1.increaseTemp());
 }
-// so basically methafood has one and other it get from food which is being extended
+if (button2) {
+    button2.addEventListener('click', () => ac1.decreaseTemp());
+}
+class food {
+    constructor() {
+        this.price = 1233;
+    }
+}
+//extend in class
+class Mithai extends food {
+    constructor() {
+        super(...arguments);
+        this.name = "kaju katli";
+    }
+}
+const mitha = new Mithai();
+mitha.name;
+mitha.price;
+//it will show like interface
+//now we will study contructor
+// in basic understanding it is a structure or skeleton method for other like ebelow
+class pendrive {
+    constructor(name) {
+        this.company = name;
+    }
+}
+let p1 = new pendrive("hp");
+class Earphones {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+}
+let e1 = new Earphones("wasay", 12);
+//so here is a short cut that we can put these variable in its contructor
+//like this
+class Earphones2 {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+        this.name = name;
+        this.price = price;
+    }
+}
+let e2 = new Earphones2("wasay", 12889);
--- script.ts
+++ script.ts
@@ -1,37 +1,116 @@
-// in interface 
-interface Human{
-    name:string;
-}
+// class object oriented paradigm
 
-interface Human{
-    age:number;
-}
-// so both things are merge together in memory
-function abcd(human:Human){
-    human.name.concat()
-    human.age.toExponential(2)
-}
-// so basically all these are merge 
-// next thing is extend
+let button1 = document.getElementById("button1")
+let button2 = document.getElementById("button2")
 
-interface Food {
-    name:string,
-    price:number
-}
-interface MethaaFood extends Food {
-verySweet:boolean
-}
-interface khattaFood extends Food {
-    verySour:boolean
+class Airpod {
+    color="white";
+    picture="image/airpod"
+    playMusic(){
+        console.log("Playing music")
     }
-function getMethaFood(mithai:MethaaFood){
-    mithai.name
-    mithai.price
-    mithai.verySweet
 }
-function getKhattaFood(khattai:khattaFood){
-  khattai.name
-  khattai.price
-  khattai.verySour
+class airconditioner {
+    color="white";
+    ton= .75;
+    temp = 22;
+
+    increaseTemp(){
+        this.temp++
+        console.log(this.temp)
+    }
+    decreaseTemp(){
+        this.temp--
+        console.log(this.temp)
+    }
+    turningOn(name:string){
+console.log(`turned on ${name} `)
+    }
 }
-// so basically methafood has one and other it get from food which is being extended
+let ac1 =new airconditioner()
+let ac2 =new airconditioner()
+
+ac1.turningOn("ac1")
+if (button1) {
+    button1.addEventListener('click',()=> ac1.increaseTemp())
+}
+if (button2) {
+    button2.addEventListener('click',()=> ac1.decreaseTemp())
+}
+
+ class food{
+    price =1233;
+ }
+//extend in class
+ class Mithai extends food {
+    name="kaju katli"
+ }
+ const mitha = new Mithai()
+
+ mitha.name
+ mitha.price
+ //it will show like interface
+
+ //now we will study contructor
+ // in basic understanding it is a structure or skeleton method for other like ebelow
+
+ class pendrive {
+    public company:string;
+constructor(name:string){
+this.company= name
+}
+ }
+
+ let p1 = new pendrive("hp")
+
+ 
+class Earphones{
+    name:string;
+    price:number
+constructor(name:string, price:number){
+    this.name = name;
+    this.price = price;
+}
+}
+let e1 = new Earphones("wasay", 12)
+//so here is a short cut that we can put these variable in its contructor
+//like this
+class Earphones2{
+    
+constructor(public name:string,public price:number){
+    this.name = name;
+    this.price = price;
+}
+}
+let e2 = new Earphones2("wasay", 12889)
+
+//access modifier
+// so for these i have left it public mean that i can change its value again mean that i have not written anything which symbolize that is will remain public
+class Earphones3{
+    name:string;
+    price:number
+constructor(name:string, price:number){
+    this.name = name;
+    this.price = price;
+}
+}
+let e3 = new Earphones2("wasay", 12)
+// public access modifier
+// private matlab sirf class main hi use hoga wo element bahaar use nahi hoga
+
+// now protected
+
+class User {
+    protected balance = 1200
+}
+class Admin extends User{
+   private isAdmin = true;
+   getBalance(){
+    this.balance
+   } 
+}
+
+let a1 = new Admin();
+//ab protected bahaar nahi use ho sakta lekin jo class use extend kar rahi hai wahan use ho jaye ga
+//private extended class may b use nahi hoga
+
